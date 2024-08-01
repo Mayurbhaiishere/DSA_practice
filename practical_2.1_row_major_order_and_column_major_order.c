d225e8bd6e2ab2a0724624ad9c14f6fceee1d0e3
Mayurbhaiishere <sunilsupaduahirrao65197.com>
2024-08-01 18:09:45 +0530

    corrected the code

---
+++ practical_2.1_row_major_order_and_column_major_order.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+  int hi[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+
+    printf("row-major order: \n");
+for(int i=0;i<4;i++){
+    for(int j=0;j<4;j++){
+        printf("%d  ",hi[i][j]);
+    }
+printf("\n");
+}
+printf("\n");
+
+
+    printf("column-major order:  \n");
+for(int i=0;i<4;i++){
+    for(int j=0;j<4;j++){
+        printf("%d  ",hi[j][i]);
+    }printf("\n");
+    }
+    return 0;
+}
