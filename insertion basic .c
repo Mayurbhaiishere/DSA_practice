4f998ab4320abd8de865d5c0ee5b1459528590d8
Mayurbhaiishere <148300992+Mayurbhaiishere@users.noreply.github.com>
2024-08-01 17:23:25 +0530

    Update insertion basic .c

--- insertion basic .c
+++ insertion basic .c
@@ -1,8 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
+int main() {
+    int a[5] = {5, 8, 10, 30, 25}; 
+    int i, j[6];
+    int insertion = 20, index = 3;
 
-int main(){
+   
+  for(i = 0; i < index; i++) {
+     j[i] = a[i];
+   }
 
+    
+    for(i = 5; i > index; i--) {
+        j[i] = a[i - 1];
+    }
+    j[index] = insertion;
+
+   
+    printf("Printing the data:\n");
+    for(i = 0; i < 6; i++) {
+        printf("%d  ", j[i]);
+    }
+    printf("\n");
 
     return 0;
 }
